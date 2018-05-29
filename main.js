3f2e091f83dcc17dff9e30a2581236066351c144
mumu111111 <2646709035@qq.com>
2018-05-29 21:19:41 +0800

    ES6 class

--- main.js
+++ main.js
@@ -1,39 +1,28 @@
-
-function Tabs(selector) {
-  this.element = $(selector)
-  this.init()
-  this.bindEvent()
+class Tabs {
+  constructor(selector) {
+    this.element = $(selector)
+    this.init()
+    this.bindEvent()
+  }
+  init() {
+    this.element.each(function (index, element) {
+      $(element).children('.tab-bar').children('li').eq(0).addClass('active').siblings().removeClass('active')
+      $(element).children('.tab-content').children('li').eq(0).addClass('active').siblings().removeClass('active')
+    })
+  }
+  bindEvent() {
+    this.element.on('click', '.tab-bar>li', function (e) {
+      var ele = e.currentTarget
+      var $li = $(ele); $li.addClass('active').siblings().removeClass('active')
+      var index = $li.index()
+      $li.closest('.tabs').find('.tab-content>li').eq(index).addClass('active')
+        .siblings().removeClass('active')
+    })
+  }
 }
 
-Tabs.prototype.init = function () {
-  //默认 情况应该显示第一个选项卡选中状态  并且 显示选项卡内容
-  this.element.each(function (index, element) {
-    $(element).children('.tab-bar').children('li').eq(0).addClass('active')
-      .siblings().removeClass('active')
-    $(element).children('.tab-content').children('li').eq(0).addClass('active')
-      .siblings().removeClass('active')
-  })
-}
-
-
-Tabs.prototype.bindEvent = function () {
-  //点击每一个选项   选项会被选中  改变边框颜色
-  this.element.on('click', '.tab-bar>li', function (e) {
-    var $li = $(e.currentTarget)
-    $li.addClass('active').siblings().removeClass('active')
-    //点击对应的选项， 显示对应第几个内容块
-    var index = $li.index()
-    //   $('.tabs').find('.tab-content>li').eq(index).addClass('active')
-    //   .siblings().removeClass('active')
-    //效果： 如果有多个选项卡  那么其他点击选项 控制的都是第一个的内容
-    $li.closest('.tabs').find('.tab-content>li').eq(index).addClass('active')
-      .siblings().removeClass('active')
-
-  })
-}
 
 var tab = new Tabs('.tabs')
-
 
 
 
